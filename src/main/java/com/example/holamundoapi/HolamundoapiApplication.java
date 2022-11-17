d2f9c4e36fe3028035959f7ee2c331b7637a592d
dalejandrov <danielvalenbf@gmail.com>
2022-11-17 18:55:38 -0500

    ajustes en main

--- src/main/java/com/example/holamundoapi/HolamundoapiApplication.java
+++ src/main/java/com/example/holamundoapi/HolamundoapiApplication.java
@@ -2,13 +2,13 @@ package com.example.holamundoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class HolamundoapiApplication {
-
+	@Profile({"dev","prod"})
 	public static void main(String[] args) {
 		SpringApplication.run(HolamundoapiApplication.class, args);
 	}
-
 }
